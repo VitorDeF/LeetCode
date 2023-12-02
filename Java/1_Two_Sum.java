e1bbc6e15331727332f6218484471b63452e9945
VitorDeF <vitorfigp@gmail.com>
2023-12-01 21:40:47 -0300

    two sum java formatting

--- Java/1_Two_Sum.java
+++ Java/1_Two_Sum.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n1, n2;
         for(int i = 0 ; i < nums.length-1 ; i++)
             for(int j = i+1 ; j < nums.length ; j++)
                 if((nums[i]+nums[j]) == target){
