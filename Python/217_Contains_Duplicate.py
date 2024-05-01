6c36adcac581569030c9e2a3088a89552b9417c1
Vítor de Figueiredo <45521318+VitorDeF@users.noreply.github.com>
2024-05-01 20:47:11 -0300

    Delete Python directory

--- Python/217_Contains_Duplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def containsDuplicate(self, nums): 
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        mapa = set(nums)
-        return len(mapa)!=len(nums)
